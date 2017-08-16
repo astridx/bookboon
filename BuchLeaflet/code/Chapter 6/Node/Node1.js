f0d2d1d04bca8011d1fa1bfbd74ff262117b46c7
astridx <info@astrid-guenther.de>
2017-08-16 17:25:20 +0200

    wip

--- BuchLeaflet/code/Chapter 6/Node/Node1.js
+++
@@ -1,6 +0,0 @@
-require('http').createServer(function (req, res) {
-	if ('/' == req.url){
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		require('fs').createReadStream('leafletessentials.html').pipe(res);
-        }
-}).listen(3000);
